90cb1df8bca4b9ac9ba3f4361d34ba7e14f4cdcf
DNL <danylo.zhalkovskyy@icloud.com>
2024-10-13 02:01:33 +0200

    :sparkles: added Navbar component

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,1 +1,22 @@
-// example
+import Image from 'next/image'
+import useTranslation from 'next-translate/useTranslation'
+import translateImg from '/public/logo-translate.png'
+import ChangeLanguage from '@/components/modules/ChangeLanguage'
+
+export default function Navbar() {
+  const { t } = useTranslation()
+
+  return (
+    <section className="grid grid-cols-3 items-center justify-center justify-items-center">
+      <div>
+        <Image src={translateImg} width={55} height={55} alt="logo-translate" />
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">{t(`common:app-title`)}</h1>
+      </div>
+      <div>
+        <ChangeLanguage />
+      </div>
+    </section>
+  )
+}
