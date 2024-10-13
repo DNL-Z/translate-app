ee6d6a25854cc8b5912d5f47047f7d7d490123d1
DNL <danylo.zhalkovskyy@icloud.com>
2024-10-13 02:01:56 +0200

    :sparkles: added ChangeLanguage component

---
+++ components/modules/ChangeLanguage.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+import useTranslation from 'next-translate/useTranslation'
+import i18nConfig from '../../i18n.json'
+
+const { locales } = i18nConfig
+
+export default function ChangeLanguage() {
+  const { t, lang } = useTranslation()
+
+  // const pathname = usePathname()
+
+  return locales.map((lng) => {
+    if (lng === lang) return null
+
+    return (
+      <Link
+        className="m-1 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        href={`/${lng}`}
+        locale={lng}
+        key={lng}
+      >
+        {t(`common:${lng}`)}
+      </Link>
+    )
+  })
+}
